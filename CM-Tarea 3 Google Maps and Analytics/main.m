34e683a6f4dd1720758fe6e078cd3495efd90ae3
joeldesantos <joeldesantos@gmail.com>
2015-07-10 18:38:57 -0500

    Initial Commit

---
+++ CM-Tarea 3 Google Maps and Analytics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CM-Tarea 3 Google Maps and Analytics
+//
+//  Created by Aleyr on 7/10/15.
+//  Copyright (c) 2015 CM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
